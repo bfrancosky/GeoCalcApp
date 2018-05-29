83bcc3d75bd1edf67ca30159931f604397277475
bfrancosky <bfrancosky@gmail.com>
2018-05-28 22:24:38 -0400

    Added fg/bg colors

---
+++ GeoCalcApp/GeoCalcTextField.swift
@@ -0,0 +1,21 @@
+//
+//  GeoCalcTextField.swift
+//  GeoCalcApp
+//
+//  Created by Brian on 5/28/18.
+//  Copyright © 2018 Brian. All rights reserved.
+//
+
+import UIKit
+
+class GeoCalcTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
